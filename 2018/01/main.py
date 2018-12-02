09acd73fcffa37a864cf22a8f9df1943c930d33e
Nicolas Hahn <nicolas@stonespring.org>
2018-12-02 10:04:56 -0500

    move day 1 main2 into main

--- 2018/01/main.py
+++ 2018/01/main.py
@@ -1,6 +1,21 @@
+# Part 1
 with open('in.txt', 'r') as f:
     ret = 0
     lines = f.readlines()
     for line in lines:
         ret += int(line)
     print(ret)
+
+
+# Part 2
+with open('in.txt', 'r') as f:
+    ret = 0
+    seen = set()
+    lines = [int(l) for l in f.readlines()]
+    while True:
+        for line in lines:
+            ret += line
+            if ret in seen:
+                print(ret)
+                exit()
+            seen.add(ret)
