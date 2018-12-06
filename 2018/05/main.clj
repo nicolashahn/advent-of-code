d5cbbddbb460d13ec8f7df97dc2a3e3b3c4aef86
Nicolas Hahn <nicolas@stonespring.org>
2018-12-06 00:29:42 -0500

    d5p2 clj

--- 2018/05/main.clj
+++ 2018/05/main.clj
@@ -4,22 +4,34 @@
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
 (def lus (map str (seq alphabet) (seq (clojure.string/upper-case alphabet))))
 (def uls (map str (seq (clojure.string/upper-case alphabet)) (seq alphabet)))
-(def all-pairs (concat lus uls))
+(def all-pairs (vec (concat lus uls)))
 
 ; remove all instances of the pair in the polymer
 (defn react-pair [poly pair]
   (clojure.string/replace poly pair ""))
 
 ; remove all instances of all pairs from the polymer
-(defn react-pairs [poly]
-  (reduce #(react-pair %1 %2) poly all-pairs))
+(defn react-pairs [poly pairs]
+  (reduce #(react-pair %1 %2) poly pairs))
 
 ; do react-pairs continually until there are no more pairs to remove
-(defn react-completely [poly]
+(defn react-completely [poly pairs]
   (def poly-atom (atom poly))
-  (while (< (count (react-pairs @poly-atom)) (count @poly-atom))
-    (swap! poly-atom react-pairs))
+  (while (< (count (react-pairs @poly-atom pairs)) (count @poly-atom))
+    (swap! poly-atom react-pairs pairs))
   @poly-atom)
 
 ; Part 1
-(println (count (react-completely polymer)))
+(println (count (react-completely polymer all-pairs)))
+
+
+; Part 2
+(println
+  (apply min 
+     (map #(count (react-completely
+                    (clojure.string/replace
+                      (clojure.string/replace 
+                        polymer (str (second %1)) "")
+                      (str (first %1)) "")
+                    all-pairs))
+          uls)))
