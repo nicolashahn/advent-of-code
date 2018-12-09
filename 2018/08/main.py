fe4494ea5b713bf793a5e5a31ce04486cb6e180f
Nicolas Hahn <nicolas@stonespring.org>
2018-12-08 17:49:32 -0800

    d8p2py

--- 2018/08/main.py
+++ 2018/08/main.py
@@ -15,8 +15,21 @@ def total_metas(node):
         total += total_metas(child)
     return total
 
+def value(node):
+    if not node['childs']:
+        return sum(node['metas'])
+    else:
+        total = 0
+        childs = node['childs']
+        for meta in node['metas']:
+            if meta in range(1,len(childs)+1):
+                total += value(childs[meta-1])
+        return total
+
 with open('in.txt', 'r') as f:
     nums = [int(n) for n in f.read().split()]
     node = get_nodes(nums)
     # Part 1
     print(total_metas(node))
+    # Part 2
+    print(value(node))
