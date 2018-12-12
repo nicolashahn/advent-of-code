51843c9b976fb5b17a9edadb1697d8d45aaec986
Nicolas Hahn <nicolas@stonespring.org>
2018-12-12 09:22:34 -0500

    day 12 p2

--- 2018/12/main.py
+++ 2018/12/main.py
@@ -24,18 +24,51 @@ def score(state, offset):
 
 with open('in.txt', 'r') as f:
     lines = f.readlines()
+    # number of negative indices
     offset = 200
     state = (['.'] * offset +
              list(lines[0].split(': ')[1].strip()) +
              ['.'] * offset)
-    # number of negative indices
     rules = set([line.split()[0] for line in lines[2:]
                  if line.split()[2] == '#'])
-    print(rules)
-    for _ in range(20):
-        # print(''.join(state))
+    for gen in range(20):
         state = step(state, offset, rules)
-    # print(''.join(state))
-
     # Part 1
     print(score(state, offset))
+
+
+with open('in.txt', 'r') as f:
+    lines = f.readlines()
+    # number of negative indices
+    offset = 2000
+    state = (['.'] * offset +
+             list(lines[0].split(': ')[1].strip()) +
+             ['.'] * offset)
+    rules = set([line.split()[0] for line in lines[2:]
+                 if line.split()[2] == '#'])
+    last_score = score(state, offset)
+    for gen in range(1000):
+        # print(''.join(state))
+        state = step(state, offset, rules)
+        new_score = score(state, offset)
+        print(gen, new_score, last_score, new_score - last_score)
+        last_score = new_score
+    # print(''.join(state))
+
+    print(score(state, offset))
+
+
+# Part 2 - some manual legwork
+# Run the thing until it finds a stable (linearly increasing) state
+# Extrapolate from that state's score, multiply the increase by 50,000,000
+# minus how many generations it took to get there and add it to the score
+'''
+>>> score = 32401  # at generation 1000
+>>> fiddy = 50000000000
+>>> fiddy - 1000
+49999999000
+>>> (fiddy - 1000) * 32
+1599999968000
+>>> (fiddy - 1000) * 32 + score
+1600000000401  # <- this was my answer
+'''
