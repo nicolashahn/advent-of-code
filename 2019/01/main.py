012be9a97ef9dbf2915237f66d9ecd11316200d4
Nicolas Hahn <nicolas@stonespring.org>
2019-11-30 21:26:23 -0800

    2019 day 1 part 2

--- 2019/01/main.py
+++ 2019/01/main.py
@@ -1,5 +1,9 @@
+def f(n):
+    return n // 3 - 2
+
+
 def p1(ns):
-    return sum([(i // 3) - 2 for i in ns])
+    return sum([f(i) for i in ns])
 
 
 assert p1([12]) == 2
@@ -8,9 +12,21 @@ assert p1([1969]) == 654
 assert p1([100756]) == 33583
 
 
+def p2(ns):
+    t = 0
+    for n in ns:
+        c = f(n)
+        while c > 0:
+            t += c
+            c = f(c)
+    return t
+
+
 def main():
     with open("in.txt", "r") as f:
-        print(p1([int(i) for i in f.readlines()]))
+        ns = [int(i) for i in f.readlines()]
+        print(p1(ns))
+        print(p2(ns))
 
 
 if __name__ == "__main__":
