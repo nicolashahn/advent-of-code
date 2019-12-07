dac1c728c864b0eb0b9d815d4ccd19bf6dae5a66
Nicolas Hahn <nicolas@stonespring.org>
2019-12-07 00:59:16 -0500

    2019 day 7 p 2

--- 2019/07/main.py
+++ 2019/07/main.py
@@ -10,8 +10,7 @@ def read_op(op):
 
 
 def execute(prog, _input):
-    prog = prog[:]
-    output = []
+    """ Generator, yields output """
     i = 0
     history = []
     while i < len(prog):
@@ -33,11 +32,12 @@ def execute(prog, _input):
             i += 4
         elif opcode == 3:
             a = prog[i + 1] if poss[0] else i + 1
+            # input is processed in reverse order
             prog[a] = _input.pop()
             i += 2
         elif opcode == 4:
             a = prog[prog[i + 1]] if poss[0] else prog[i + 1]
-            output.append(a)
+            yield a
             i += 2
         elif opcode == 5:
             a, b = [
@@ -80,10 +80,18 @@ def execute(prog, _input):
                 )
             )
             __import__("pdb").set_trace()
-    return output
 
 
-def amps(prog):
+def execute_collect(prog, _input):
+    """ Collects output into a list """
+    gen = execute(prog, _input)
+    out = []
+    for output in gen:
+        out.append(output)
+    return out
+
+
+def p1(prog):
     combos = permutations([0, 1, 2, 3, 4], 5)
     best = 0
     for combo in combos:
@@ -91,15 +99,37 @@ def amps(prog):
         _in = [0]
         for _ in range(5):
             _in.append(combo.pop())
-            _in = execute(prog, _in)
+            _in = execute_collect(prog[:], _in)
         best = max(best, _in[0])
+    return best
+
+
+def p2(prog):
+    combos = permutations([5, 6, 7, 8, 9], 5)
+    best = 0
+    for combo in combos:
+        combo = list(combo)
+        ins = [[combo[i]] for i in range(5)]
+        ins[0].insert(0, 0)
+        exes = [execute(prog[:], _in) for _in in ins]
+        ie = 0
+        while any(exes):
+            try:
+                out = next(exes[ie])
+                ins[(ie + 1) % 5].insert(0, out)
+            except StopIteration:
+                exes[ie] = None
+            ie += 1
+            ie %= 5
+        best = max(ins[0][0], best)
     return best
 
 
 def main():
     with open("in.txt", "r") as f:
         prog = [int(i) for i in f.readlines()[0].split(",")]
-        print(amps(prog))
+        print(p1(prog))
+        print(p2(prog))
 
 
 if __name__ == "__main__":
