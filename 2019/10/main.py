3904998cdcfe4661414ab1f81d5a79216305d1f9
Nicolas Hahn <nicolas@stonespring.org>
2019-12-10 10:27:25 -0500

    2019 day 10 p2

--- 2019/10/main.py
+++ 2019/10/main.py
@@ -1,3 +1,7 @@
+def parse_in(raw):
+    return [list(l.strip()) for l in raw.split()]
+
+
 class Ratio:
     def __init__(self, rad, div):
         rneg, dneg = rad < 0, div < 0
@@ -24,16 +28,17 @@ assert Ratio(-7, 9).pair == (-7, 9)
 assert Ratio(7, -9).pair != (-7, 9)
 
 
-def count(space, sx, sy):
-    asteroids = []
+def get_vis(space, sx, sy):
+    asts = []
     for y, row in enumerate(space):
         for x, c in enumerate(row):
             if c == "#" and ((x, y) != (sx, sy)):
-                asteroids.append((x, y))
+                asts.append((x, y))
     blocked = set()
-    ct = 0
-    s_asteroids = sorted(asteroids, key=lambda a: abs(sx - a[0]) + abs(sy - a[1]))
-    for ax, ay in s_asteroids:
+    visible = set()
+    # sorted by dist to sx,sy
+    sort_asts = sorted(asts, key=lambda a: abs(sx - a[0]) + abs(sy - a[1]))
+    for ax, ay in sort_asts:
         if (ax, ay) in blocked:
             continue
         # d for distance
@@ -44,36 +49,119 @@ def count(space, sx, sy):
             bx += dx
             by += dy
             blocked.add((bx, by))
-        ct += 1
-    return ct
+        visible.add((ax, ay))
+    return visible
 
 
 def p1(space):
     best = 0
+    bxy = None
     for y, row in enumerate(space):
         for x, c in enumerate(row):
             if c == "#":
-                ct = count(space, x, y)
-                best = max(best, ct)
-    return best
+                ct = len(get_vis(space, x, y))
+                if best < ct:
+                    best = ct
+                    bxy = x, y
+    return best, bxy
 
 
-test = [
-    l.strip()
-    for l in """.#..#
+test_space = parse_in(
+    """.#..#
 .....
 #####
 ....#
-...##""".split()
-]
+...##"""
+)
 
-assert p1(test) == 8
+
+assert p1(test_space)[0] == 8
+
+
+def laser(space, sx, sy, vis):
+    # the asteroids that were destroyed in this rotation in order
+    batch = []
+    # relative position compared to base (sx,sy)
+    rpos = [(x - sx, y - sy) for x, y in vis]
+    # upper-right quadrant, processed 1st
+    urq = [(x, y) for x, y in rpos if y < 0 and x >= 0]
+    # lower-right, processed 2nd
+    lrq = [(x, y) for x, y in rpos if y >= 0 and x > 0]
+    # lower-left, processed 3rd
+    llq = [(x, y) for x, y in rpos if y > 0 and x <= 0]
+    # upper-left, processed 4th
+    ulq = [(x, y) for x, y in rpos if y <= 0 and x < 0]
+    assert len(urq + lrq + llq + ulq) == len(vis)
+
+    def comp(xy):
+        x, y = xy
+        if x == 0:
+            return float("inf")
+        return float(abs(y)) / float(abs(x))
+
+    # put all quadrants in sorted order if rotating clockwise
+    surq = sorted(urq, key=comp, reverse=True)
+    slrq = sorted(lrq, key=comp)
+    sllq = sorted(llq, key=comp, reverse=True)
+    sulq = sorted(ulq, key=comp)
+    # start blasting
+    for quad in (surq, slrq, sllq, sulq):
+        for x, y in quad:
+            # convert relative back to absolute pos
+            ax, ay = sx + x, sy + y
+            batch.append((ax, ay))
+            space[ay][ax] = "."
+    return batch
+
+
+def p2(space, x, y):
+    # list of destroyed asteroids in order of destruction
+    ds = []
+    # this list comp flattens space to a 1d list
+    while "#" in [p for row in space for p in row]:
+        vis = get_vis(space, x, y)
+        batch = laser(space, x, y, vis)  # mutates space
+        if not batch:
+            break
+        ds += batch
+        if len(ds) >= 200:
+            ax, ay = ds[199]
+            return ax * 100 + ay
+    print("p2 broken")
+
+
+test_space2 = parse_in(
+    """.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##"""
+)
+
+assert p2(test_space2, 11, 13) == 802
 
 
 def main():
     with open("in.txt", "r") as f:
-        space = [l.strip() for l in f.readlines()]
-        print(p1(space))
+        space = parse_in(f.read())
+        p1_ans, xy = p1(space)
+        assert p1_ans == 319
+        assert p2(space, *xy) == 517
 
 
 if __name__ == "__main__":
