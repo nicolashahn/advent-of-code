b5875cf7ec180b56db39d15ecdf686d3d6af6076
Nicolas Hahn <nicolas@stonespring.org>
2019-12-18 10:14:08 -0500

    forgot to commit 2019 day 16 p2

--- 2019/16/main.py
+++ 2019/16/main.py
@@ -19,6 +19,21 @@ def p1(seq, phases):
     return seq[:8]
 
 
+def p2(seq, phases):
+    seq = list(seq * 10000)
+    # return
+    r_offset = len(seq) - int("".join(seq[:7]))
+    for pi in range(phases):
+        s = 0
+        for i in range(r_offset):
+            s += int(seq[~i])
+            if s > 10:
+                s -= 10
+            seq[~i] = s
+            if pi == phases - 1 and i >= r_offset - 8:
+                print(s)
+
+
 assert p1("12345678", 1) == "48226158"
 assert p1("12345678", 4) == "01029498"
 assert p1("80871224585914546619083218645595", 100) == "24176176"
@@ -27,7 +42,8 @@ assert p1("80871224585914546619083218645595", 100) == "24176176"
 def main():
     with open("in.txt", "r") as f:
         seq = f.read().strip()
-        print(p1(seq, 100))
+        # print(p1(seq, 100))
+        print(p2(seq, 100))
 
 
 if __name__ == "__main__":
